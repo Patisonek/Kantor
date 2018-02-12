65aa46b8930715d73a9f89ab8cdc59f4f927d3cd
Patisonek <patrycja.warmuz@gmail.com>
2018-02-12 16:09:30 +0100

    implementet done button

--- Kantor/ExchangeViewController.swift
+++ Kantor/ExchangeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ExchangeViewController: UIViewController {
+class ExchangeViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
@@ -19,13 +19,18 @@ class ExchangeViewController: UIViewController {
     
     @IBOutlet weak var exchangeLabel: UILabel!
     
-    @IBOutlet weak var hideUserInputsButton: UIBarButtonItem!
+    @IBOutlet  var hideUserInputsButton: UIBarButtonItem!
     
+    @IBAction func actionHideUserInputs(_ sender: Any) {
+        userAmountTextField.resignFirstResponder()
+        navigationItem.rightBarButtonItem = nil
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        userAmountTextField.text = "100"
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +38,11 @@ class ExchangeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        navigationItem.rightBarButtonItem = hideUserInputsButton
+    }
+    
+    
 
 }
 
