560990a534e451a4509c62df450c2d0c0a75b796
Patisonek <patrycja.warmuz@gmail.com>
2018-02-20 13:58:52 +0100

    add struct Currency

--- Kantor/Models/Kantor.swift
+++ Kantor/Models/Kantor.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
+struct Currency {
+    
+    var code: String
+    var name: String
+    var buy: Float
+    var sell: Float
+    var conversion: Float
+    
+}
+
+
 class Kantor: NSObject {
+    
+    var currencies: [String: Currency] = [:]
     
     override init() {
         
@@ -22,6 +35,17 @@ class Kantor: NSObject {
                 
                 for item in list {
                     print("item: \(item)")
+                    
+                    var name = item["name"] as! String
+                    var code = item["code"] as! String
+                    var buy = item["buy"] as! Float
+                    var sell = item["sell"] as! Float
+                    var conversion = item["conversion"] as! Float
+                    
+                    let curr = Currency(code: code, name: name, buy: buy, sell: sell, conversion: conversion )
+                    
+                    currencies.updateValue(curr, forKey: code)
+                    
                 }
             }
             else {
